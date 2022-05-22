e33f17af69d28f37d18c17c590b4da2fdb7d95c1
SyedzadaAbrar <105579901+SyedzadaAbrar@users.noreply.github.com>
2022-05-22 08:50:42 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -4,3 +4,7 @@ echo -e "\e[35mmade by syed abrar\e[0m";
 ls welcome
 cd  mkdir
 echo -e "\e[36mwelcome\e[0m";
+pip3 install pyautogui
+pip3 install webbot
+
+git clone https://github.com/Khanejo/Instagram-reporter
